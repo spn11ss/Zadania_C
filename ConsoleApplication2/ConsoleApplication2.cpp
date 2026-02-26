91d17dd98b88163651c444b13dc275702e93271d
ника <stepanecnikusa@gmail.com>
2026-02-26 15:05:53 +0300

    подключение библиотек и установка кодировки для русского языка

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <windows.h>
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_FILENAME_LENGTH 256
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    
+}
